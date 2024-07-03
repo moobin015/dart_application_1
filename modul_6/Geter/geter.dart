dd2aea1db78fd24efc1b11fbea8a67d557b68411
moobin <moobinislam3@gmail.com>
2024-07-03 12:06:42 +0600

    commit

---
+++ modul_6/Geter/geter.dart
@@ -0,0 +1,9 @@
+class First {
+  String name = 'BMI';
+  String post = 'Enginner';
+  double _selary = 145000.50;
+
+  double get get_selary {
+    return _selary;
+  }
+}
---
+++ modul_6/Geter/main.dart
@@ -0,0 +1,9 @@
+import 'geter.dart';
+
+void main (){
+  First moobin= First();
+  print(moobin.name);
+  print(moobin.post);
+  print(moobin.get_selary);
+  ///moobin.get_selary= 455556.5;
+}
